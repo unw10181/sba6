2b99765c5cc908e8ba62a5ccd2623acefa907e5d
unw10181 <uchennanwilliams@gmail.com>
2025-11-25 11:17:01 -0500

    task1

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,3 +1,39 @@
+import { calculateDiscount } from "../utils/discountCalculator";
+
 export class Product {
-    
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+
+  constructor(
+    id: number,
+    title: string,
+    description: string,
+    category: string,
+    price: number,
+    discountPercentage: number,
+    rating: number
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.category = category;
+    this.price = price;
+    this.discountPercentage = discountPercentage;
+    this.rating = rating;
+  }
+
+  getPriceWithDiscount = (): number => {
+    const discount = calculateDiscount(this.price, this.discountPercentage)
+    return this.price -= discount;
+  }
+
+  displayDetails = (): string => {
+    return `Product: ${this.title} Price: ${this.price} Description: ${this.description} Rating: ${this.rating}`
+  }
+
 }
---
+++ src/utils/discountCalculator.ts
@@ -0,0 +1,5 @@
+
+
+export function calculateDiscount(price: number, discountAmount: number): number {
+    return price *= discountAmount;
+}
