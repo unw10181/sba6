2a98aa07ec1b2a2f2d46ecd3f6581055285a13bc
unw10181 <uchennanwilliams@gmail.com>
2025-11-25 11:48:50 -0500

    task2

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -8,6 +8,7 @@ export class Product {
   price: number;
   discountPercentage: number;
   rating: number;
+  taxPercentage: number;
 
   constructor(
     id: number,
@@ -16,24 +17,28 @@ export class Product {
     category: string,
     price: number,
     discountPercentage: number,
-    rating: number
+    rating: number,
+    taxPercentage: number = 0.0475
   ) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.category = category;
     this.price = price;
+    if (category == "grocery") {
+      discountPercentage == 0.03;
+    }
     this.discountPercentage = discountPercentage;
     this.rating = rating;
+    this.taxPercentage = taxPercentage;
   }
 
   getPriceWithDiscount = (): number => {
-    const discount = calculateDiscount(this.price, this.discountPercentage)
-    return this.price -= discount;
-  }
+    const discount = calculateDiscount(this.price, this.discountPercentage);
+    return (this.price -= discount);
+  };
 
   displayDetails = (): string => {
-    return `Product: ${this.title} Price: ${this.price} Description: ${this.description} Rating: ${this.rating}`
-  }
-
+    return `Product: ${this.title} Price: ${this.price} Description: ${this.description} Rating: ${this.rating}`;
+  };
 }
